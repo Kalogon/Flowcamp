d4a24557f3561da4ad0c9ea5226a00e7f0db05d2
이건호 <ghlee5084@naver.com>
2019-12-28 22:02:44 +0900

    add map

--- Madcamp/app/src/main/java/com/example/madcamp/FragmentThree.java
+++ Madcamp/app/src/main/java/com/example/madcamp/FragmentThree.java
@@ -10,10 +10,19 @@ import androidx.fragment.app.Fragment;
 
 import com.example.madcamp.R;
 
+import net.daum.mf.map.api.MapView;
+
 public class FragmentThree extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_layout_3,container,false);
+        View view=inflater.inflate(R.layout.mapview,container,false);
+
+        MapView mapView = new MapView(this.getActivity());
+
+        ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+
+        return view;
     }
 }
