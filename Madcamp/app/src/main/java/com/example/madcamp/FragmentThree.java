555945fecabc0e5b411d894f6dbb5eef7b728b92
dahee <d2634@naver.com>
2019-12-30 14:51:10 +0900

    daheetest

--- Madcamp/app/src/main/java/com/example/madcamp/FragmentThree.java
+++ Madcamp/app/src/main/java/com/example/madcamp/FragmentThree.java
@@ -10,18 +10,18 @@ import androidx.fragment.app.Fragment;
 
 import com.example.madcamp.R;
 
-import net.daum.mf.map.api.MapView;
+//import net.daum.mf.map.api.MapView;
 
 public class FragmentThree extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.mapview,container,false);
+        View view=inflater.inflate(R.layout.frag_layout_3,container,false);
 
-        MapView mapView = new MapView(this.getActivity());
+        /*MapView mapView = new MapView(this.getActivity());
 
         ViewGroup mapViewContainer = (ViewGroup) view.findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);
+        mapViewContainer.addView(mapView);*/
 
         return view;
 
